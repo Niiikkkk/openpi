34b326c069a5a4f9881e93f9b76b5cfbe8ccc83d
adminalp <nicholas.berardo.00@gmail.com>
2026-08-06 22:02:53 +0200

    changes

--- src/openpi/training/config.py
+++ src/openpi/training/config.py
@@ -818,7 +818,7 @@ _CONFIGS = [
             base_config=DataConfig(prompt_from_task=True),
         ),
         weight_loader=weight_loaders.CheckpointWeightLoader("gs://openpi-assets/checkpoints/pi0_base/params"),
-        num_train_steps=10_000,
+        num_train_steps=5_000,
         # The freeze filter defines which parameters should be frozen during training.
         # We have a convenience function in the model config that returns the default freeze filter
         # for the given model config for LoRA finetuning. Just make sure it matches the model config
@@ -830,6 +830,7 @@ _CONFIGS = [
         ema_decay=None,
         wandb_enabled=False,
         batch_size=32,
+        keep_period=1000,
     ),
     TrainConfig(
         # Change the name to reflect your model and dataset.
